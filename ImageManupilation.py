779e7767e00c4f0ddb7d07ff1fa2d4d2f4d63ce4
Oğuz Kaan Oğuz <oguzhan746@hotmail.com>
2023-03-07 16:19:57 +0300

    fixed 1.0.1

--- ImageManupilation.py
+++ ImageManupilation.py
@@ -1,1 +1,20 @@
+from PIL import Image, ImageFilter
+
+def resize_image(image_path,width,height):
+    """ Resize an image to the specified width and height."""
+    with Image.open(image_path) as img:
+        resized = img.resize((width,height))
+        resized.save('resized.jpg')
+
+def apply_filter(image_path,filter_name):
+    """ Apply a filter to an image."""
+    with Image.open(image_path) as img:
+        filtered = img.filter(filter_name)
+        filtered.save('filtered.jpg')
+
+# Usage
+image_path ='example.jpg'
+resize_image(image_path, 640x480)
+apply_filter(image_path, ImageFilter.SHARPEN)
+
 
